3cb607baeb0d8f87d1df9edd5818912039cc77e8
EliHimself <tamosauskas09@gmail.com>
2023-10-04 18:17:41 +0300

    hit the api

--- pages/index.js
+++ pages/index.js
@@ -2,43 +2,43 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-import { getSortedPostsData } from "../lib/posts";
-
 import Link from "next/link";
 import Date from "../components/date";
+import { API_URL, TOKEN } from "../consts";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const response = await fetch(`${API_URL}/posts`, {
+    headers: { Authorization: TOKEN },
+  });
+
+  const posts = await response.json();
+
   return {
     props: {
-      allPostsData,
+      posts: posts.data,
     },
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ posts }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I am a very cool developer!</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>I am a very cool developer with a 💩 blog!</p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {posts.map(({ id, attributes: { Title, publishedAt } }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
+              <Link href={`/posts/${id}`}>{Title}</Link>
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
+                <Date dateString={publishedAt} />
               </small>
             </li>
           ))}
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,39 +1,55 @@
 import Layout from "../../components/layout";
 import Head from "next/head";
 
-import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
+import { API_URL, TOKEN } from "../../consts";
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+  const response = await fetch(`${API_URL}/posts/${params.id}`, {
+    headers: { Authorization: TOKEN },
+  });
+
+  const postData = await response.json();
+
   return {
     props: {
-      postData,
+      postData: postData.data,
     },
   };
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds();
+  const response = await fetch(`${API_URL}/posts`, {
+    headers: { Authorization: TOKEN },
+  });
+
+  const posts = await response.json();
+
+  const paths = posts.data.map((post) => ({ params: { id: String(post.id) } }));
+
   return {
     paths,
     fallback: false,
   };
 }
 
-export default function Post({ postData }) {
+export default function Post({
+  postData: {
+    attributes: { Title, Content, publishedAt },
+  },
+}) {
   return (
     <Layout>
       <Head>
-        <title>{postData.title}</title>
+        <title>{Title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <h1 className={utilStyles.headingXl}>{Title}</h1>
         <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
+          <Date dateString={publishedAt} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div>{Content}</div>
       </article>
     </Layout>
   );
